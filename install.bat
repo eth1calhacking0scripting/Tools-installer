f13e757e6dd5938fa4166a808893e0a1ad2ab019
eth1calhacking0scripting <154705030+eth1calhacking0scripting@users.noreply.github.com>
2024-01-05 18:10:30 +0300

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,8 @@
+echo INSTALLING THE REQUIREMENT
+echo IT WILL AUTOMATICALLY RUN SCRIPT AFTER INSTALLED PACKAGES
+pkg update
+pkg upgrade
+pip install colorama
+pip install python2
+pip install python3
+python tool.py
---
+++ main.py
@@ -0,0 +1,199 @@
+import os
+from colorama import Fore
+os.system("clear")
+print(Fore.BLUE + """
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+|T|o|o|l|s|-|I|n|s|t|a|l|l|e|r|
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+1. Hacking tools / anonymous tools-(to hide yourself)
+2. exit
+3. contact(if you have any problem with tool or need help)
+[---------------]
+[-------]
+'Please use this tools for ethical hacking and not harming anyone'
+'i'm not responsible for any of your actions or damage'
+[-------]
+[-------]
+Root: No
+Version: 0.1
+For Windows: No
+For Kali Linux: No
+""")
+print(Fore.YELLOW + "Github~$: https://github.com/eth1calhacking0scripting")
+print(Fore.YELLOW + "Discord~$: real39203#0000")
+c = input("~$: ")
+if c == "1":
+   print(Fore.CYAN + """
+[-------------]
+1. PROXY GENERATOR
+2. TORSOCKS
+3. PROXYCHAINS
+4. ANONSURF
+5. ANOMLY(ANONYMOUS CHAT)
+6. SQLMAP
+7. NMAP(network scanning. port scanning etc)
+8. METASPLOIT (testing android security etc)
+9. HYDRA
+10. ADMINHACK
+11. ZPHISHING TOOL
+12. WIFIPHISHER
+13. IP-TRACKER
+14. FACEBOOK BRUTEFORCE
+15. INSTAGRAM BRUTEFORCE
+16. XATTACKER TOOL
+17. DDOS-RIPPER
+18. TROJAN VIRUS CREATOR TOOL(FOR UBUNTU AND TERMUX)
+19. SHERLOCK(HUNT ACCOUNTS BY USERNAME)
+20. MHDDOS
+21. STORM-BREAKER
+22. MASKPHISH
+23. SOCIALPWNED
+24. FLUXER
+25. XSSTRIKE
+26. EASY_HACK
+27. TRAPE
+28. CUPP(THIS CAN BE USED FOR PASSWORD-BRUTEFORCING)
+29. RECON
+30. SPIDERFOOT
+31. SNIPERPHISHING
+32. AIRCRACK-NG
+33. VBUG(MALWARES MAKER)
+34. PHONEINFOGA
+35. MOSINT
+36. STRIKE
+37. PYPHISHER
+38. USERFINDER
+39. CC-ATTACK
+40. ZERO-ATTACKER
+""")
+elif c == "2":
+ exit(0)
+elif c == "3":
+ os.system("termux-open-url https://t.me/ethicalperson")
+######
+c_2 = input("~$: ")
+if c_2 == "1":
+    os.system("figlet INSTALLING...")
+    os.system("git clone https://github.com/wildyrando/Get-Free-Proxy")
+elif c_2 == "2":
+   os.system("figlet INSTALLING...")
+   os.system("git clone https://github.com/dgoulet/torsocks")
+elif c_2 == "3":
+   os.system("figlet INSTALLING...")
+   os.system("git clone https://github.com/haad/proxychains")
+elif c_2 == "4":
+   os.system("figlet INSTALLING...")
+   os.system("git clone  https://github.com/machine1337/anonsurf")
+elif c_2 == "5":
+   os.system("figlet INSTALLING...")
+   os.system("git clone https://github.com/msk4862/Anomly")
+elif c_2 == "6":
+   os.system("figlet INSTALLING...")
+   os.system("git clone https://github.com/sqlmapproject/sqlmap")
+elif c_2 == "7":
+   os.system("figlet INSTALLING...")
+   os.system("git clone https://github.com/nmap/nmap")
+elif c_2 == "8":
+   os.system("figlet INSTALLING...")
+   os.system("git clone https://github.com/gushmazuko/metasploit_in_termux")
+elif c_2 == "9":
+   os.system("figlet INSTALLING...")
+   os.system("git clone https://github.com/iamunixtz/Termux-Hydra")
+elif c_2 == "10":
+   os.system("figlet INSTALLING...")
+   os.system("git clone https://github.com/mishakorzik/AdminHack")
+elif c_2 == "11":
+   os.system("figlet INSTALLING...")
+   os.system("git clone https://github.com/htr-tech/zphisher")
+elif c_2 == "12":
+   os.system("figlet INSTALLING...")
+   os.system("git clone https://github.com/wifiphisher/wifiphisher")
+elif c_2 == "13":
+   os.system("figlet INSTALLING...")
+   os.system("git clone https://github.com/rajkumardusad/IP-Tracer")
+elif c_2 == "14":
+   os.system("figlet INSTALLING...")
+   os.system("git clone https://github.com/IAmBlackHacker/Facebook-BruteForce")
+elif c_2 == "15":
+   os.system("figlet INSTALLING...")
+   os.system("git clone https://github.com/STARK-404/INSTA")
+elif c_2 == "16":
+   os.system("figlet INSTALLING...")
+   os.system("git clone https://github.com/Moham3dRiahi/XAttacker")
+elif c_2 == "17":
+   os.system("figlet INSTALLING...")
+   os.system("git clone https://github.com/palahsu/DDoS-Ripper")
+elif c_2 == "18":
+   os.system("figlet INSTALLING...")
+   os.system("git clone https://github.com/termuxhackers-id/SARA")
+elif c_2 == "19":
+   os.system("figlet INSTALLING...")
+   os.system("git clone https://github.com/sherlock-project/sherlock")
+elif c_2 == "20":
+   os.system("figlet INSTALLING...")
+   os.system("git clone https://github.com/MatrixTM/MHDDoS")
+elif c_2 == "21":
+   os.system("figlet INSTALLING...")
+   os.system("git clone https://github.com/ultrasecurity/Storm-Breaker")
+elif c_2 == "22":
+   os.system("figlet INSTALLING...")
+   os.system("git clone https://github.com/jaykali/maskphish")
+elif c_2 == "23":
+  os.system("INSTALLING...")
+  os.system("git clone https://github.com/MrTuxx/SocialPwned")
+elif c_2 == "24":
+  os.system("figlet INSTALLING...")
+  os.system("git clone https://github.com/0n1cOn3/FluxER")
+elif c_2 == "25":
+  os.system("figlet INSTALLING...")
+  os.system("git clone https://github.com/s0md3v/XSStrike")
+elif c_2 == "26":
+  os.system("figlet INSTALLING...")
+  os.system("git clone https://github.com/sabri-zaki/EasY_HaCk")
+elif c_2 == "27":
+  os.system("figlet INSTALLING...")
+  os.system("git clone https://github.com/jofpin/trape")
+elif c_2 == "28":
+  os.system("figlet INSTALLING...")
+  os.system("git clone https://github.com/Mebus/cupp")
+elif c_2 == "29":
+  os.system("figlet INSTALLING...")
+  os.system("git clone https://github.com/Techzindia/recon-ng_termux_by_viral")
+elif c_2 == "30":
+  os.system("figlet INSTALLING...")
+  os.system("git clone https://github.com/smicallef/spiderfoot")
+elif c_2 == "31":
+  os.system("figlet INSTALLING...")
+  os.system("git clone https://github.com/GemGeorge/SniperPhish")
+elif c_2 == "32":
+  os.system("figlet INSTALLING...")
+  os.system("git clone https://github.com/aircrack-ng/aircrack-ng")
+elif c_2 == "33":
+  os.system("figlet INSTALLING...")
+  os.system("git clone https://github.com/Gameye98/vbug")
+elif c_2 == "34":
+  os.system("figlet INSTALLING...")
+  os.system("git clone https://github.com/sundowndev/phoneinfoga")
+elif c_2 == "35":
+  os.system("figlet INSTALLING...")
+  os.system("git clone https://github.com/alpkeskin/mosint")
+elif c_2 == "36":
+  os.system("figlet INSTALLING...")
+  os.system("git clone https://github.com/s0md3v/Striker")
+elif c_2 == "37":
+  os.system("figlet INSTALLING...")
+  os.system("git clone https://github.com/KasRoudra/PyPhisher")
+elif c_2 == "38":
+  os.system("figlet INSTALLING...")
+  os.system("git clone https://github.com/mishakorzik/UserFinder")
+elif c_2 == "39":
+  os.system("figlet INSTALLING...")
+  os.system("git clone https://github.com/Leeon123/CC-attack")
+elif c_2 == "40":
+  os.system("figlet INSTALLING...")
+  os.system("git clone https://github.com/AsjadOooO/Zero-attacker")
+
+
+######
+else:
+ print(Fore.RED + "INVALID")
